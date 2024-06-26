27ca5a2883a1e47f5faf14554b2e6144ed44cabd
wangkai <1287345523@qq.com>
2024-06-26 08:36:33 +0800

    FEAT：生成实体类与repository

--- codegeneration/index.js
+++ codegeneration/index.js
@@ -1,5 +1,7 @@
 var inputObject = document.getElementsByName("inputBox")[0];
 var analyseObject = document.getElementsByName("analyseResultBox")[0];
+var entityObject = document.getElementsByName("entityResultBox")[0];
+var repositoryObject = document.getElementsByName("repositoryResultBox")[0];
 /**
  * 0:CREATE TABLE
  * 1:(
@@ -12,10 +14,78 @@ var analyseObject = document.getElementsByName("analyseResultBox")[0];
  * 8:CONSTRAINT
  * 9:)
  * 10:MS_Description
+ * 11:@value
+ * 12:@level2name
+ * 13:'
+ * 14:"[[Table]]"	标识这是表格相关信息
+ * 15:_	表名的下划线分隔符
+ * 16:Id
  */
-var keyWords = ["CREATE TABLE","(",".","[","]"," ",",","?","CONSTRAINT",")","MS_Description"]
+var keyWords = ["CREATE TABLE","(",".","[","]"," ",",","?","CONSTRAINT",")","MS_Description","@value","@level2name","'","[[Table]]","_","Id"]
 inputObject.onchange=inputFunction;
+var type = { 
+	
+bigint: 'long',
+binary: 'byte[]',
+bit: 'bool',
+char: 'string',
+date: 'DateTime',
+datetime: 'DateTime',
+datetime2: 'DateTime',
+datetimeoffset: 'DateTimeOffset',
+decimal: 'decimal',
+float: 'float',
+image: 'byte[]',
+int: 'int',
+money: 'decimal',
+nchar: 'char',
+ntext: 'string',
+numeric: 'decimal',
+nvarchar: 'string',
+real: 'double',
+smalldatetime: 'DateTime',
+smallint: 'short',
+smallmoney: 'decimal',
+text: 'string',
+time: 'TimeSpan',
+timestamp: 'DateTime',
+tinyint: 'byte',
+uniqueidentifier: 'Guid',
+varbinary: 'byte[]',
+varchar: 'string',
+    
 
+"bigint?": 'long?',
+"binary?": 'byte[]?',
+"bit?": 'bool?',
+"char?": 'string',
+"date?": 'DateTime?',
+"datetime?": 'DateTime?',
+"datetime2?": 'DateTime?',
+"datetimeoffset?": 'DateTimeOffset?',
+"decimal?": 'decimal?',
+"float?": 'float?',
+"image?": 'byte[]?',
+"int?": 'int?',
+"money?": 'decimal?',
+"nchar?": 'char?',
+"ntext?": 'string',
+"numeric?": 'decimal?',
+"nvarchar?": 'string',
+"real?": 'double?',
+"smalldatetime?": 'DateTime?',
+"smallint?": 'short?',
+"smallmoney?": 'decimal?',
+"text?": 'string',
+"time?": 'TimeSpan?',
+"timestamp?": 'DateTime?',
+"tinyint?": 'byte?',
+"uniqueidentifier?": 'Guid?',
+"varbinary?": 'byte[]?',
+"varchar?": 'string'
+}
+
+inputFunction();
 function inputFunction(){
 	var inputContent = inputObject.value;
 	
@@ -24,16 +94,159 @@ function inputFunction(){
 	output += result.tableName;
 	output+="\n\n字段名\t\t\t\t\t\t字段类型\t\t\t\t\t可空\n";
 	var fieldInfo = result.fieldInfo;
+	var fieldDescription = result.fieldDescription;
 	if(fieldInfo!=undefined){
 		for (let index = 0; index < fieldInfo.length; index++) {
 			const element = fieldInfo[index];
-			element.fieldName = addBlackSpace(element.fieldName);
-			element.fieldType = addBlackSpace(element.fieldType);
-			output=output+"\n"+ element.fieldName+element.fieldType+element.fieldIsNullAble;
+			var fieldName = addBlackSpace(element.fieldName);
+			var fieldType = addBlackSpace(element.fieldType);
+			output=output+"\n"+ fieldName+fieldType+element.fieldIsNullAble;
+			output = output + "\n字段注释："+fieldDescription[element.fieldName];
 		}
 	}
-	
 	analyseObject.value=output;
+	var fileName = result.tableName;
+	var fileNameIndex = result.tableName.indexOf(keyWords[15]);
+	if(fileNameIndex>-1){
+		fileName = result.tableName.substring(fileNameIndex+keyWords[15].length);
+	}
+	// 生成实体类
+	var fieldRegenerationInfo = getEntityCode(fileName,result.tableName, fieldInfo,fieldDescription);
+	entityObject.value=fieldRegenerationInfo.entityCode;
+	// 生成repository
+	var repositoryCode = getRepositoryCode(fileName, result.tableName, fieldRegenerationInfo);
+	repositoryObject.value=repositoryCode;
+}
+
+function getEntityCode(tableNameWithoutFrontWord,tableName, fieldInfo,fieldDescription,namespace="ZOS.Common.Entities"){
+	var maxRowLength=120;
+	var getNettRowStart = "\n\t\t\t\t";
+	var addFieldSql=`${getNettRowStart}INSERT INTO ${tableName} (`;
+	var addValueSql=`VALUES (`;
+	var updateSql=`${getNettRowStart}UPDATE ${tableName} SET `;
+	var querySql=`${getNettRowStart}SELECT `;
+	var sqlCurrentRowLength ={
+		addField:addFieldSql.length,
+		addValue:addValueSql.length,
+		update:updateSql.length,
+		query: querySql.length
+	} 
+	var entityCode=`using System;
+using ZOS.Common.Entities.Enums.ProduceManagement;
+using ZOS.Data;
+
+namespace ${namespace}
+{
+	/// <summary>
+	/// ${fieldDescription[keyWords[14]]}
+	/// </summary>
+	public class ${tableNameWithoutFrontWord} : Entity
+	{`;
+	if(fieldInfo!=undefined){
+		for (let index = 0; index < fieldInfo.length; index++) {
+			var commaSign = index===fieldInfo.length-1?"":", ";
+			const element = fieldInfo[index];
+			var fieldName = element.fieldName;
+			var fieldType = element.fieldType;
+			var fieldIsNullAble = element.fieldIsNullAble;
+
+			// 实体类字段
+			if(fieldName !== keyWords[16]){
+				entityCode=entityCode +`
+			
+		/// <summary>
+		/// ${fieldDescription[fieldName] == undefined?"":fieldDescription[fieldName]}
+		/// </summary>
+		public ${type[fieldType+fieldIsNullAble] == undefined ? fieldType : type[fieldType+fieldIsNullAble]} ${fieldName} { get; set; }`;
+			}
+
+			// repository 内容
+			var withSignField = `@${fieldName}${commaSign}`;
+			var withoutSignField = `[${fieldName}]${commaSign}`;
+			if(sqlCurrentRowLength.addField+withoutSignField.length>maxRowLength){
+				sqlCurrentRowLength.addField = withoutSignField.length;
+				addFieldSql+=getNettRowStart;
+				addValueSql+=getNettRowStart;
+			}else{
+				sqlCurrentRowLength.addField += withoutSignField.length;
+			}
+			addFieldSql=addFieldSql+`${withoutSignField}`;
+			addValueSql=addValueSql+`${withSignField}`;
+			
+			if(sqlCurrentRowLength.query+withoutSignField.length>maxRowLength){
+				sqlCurrentRowLength.query = withoutSignField.length;
+				querySql+=getNettRowStart;
+			}else{
+				sqlCurrentRowLength.query += withoutSignField.length;
+			}
+			querySql=querySql+`${withoutSignField}`;
+			
+			var updateField = `[${fieldName}] = @${fieldName}${commaSign}`;
+			if(sqlCurrentRowLength.update+updateField.length>maxRowLength){
+				sqlCurrentRowLength.update = updateField.length;
+				updateSql+=getNettRowStart;
+			}else{
+				sqlCurrentRowLength.update += updateField.length;
+			}
+			updateSql=updateSql+`${updateField}`;
+			
+			
+		}
+	}
+	addFieldSql+=`)${getNettRowStart}`;
+	addValueSql+=`) `;
+	updateSql+=`${getNettRowStart}WHERE [Id] = @Id `;
+	querySql += `${getNettRowStart}FROM ${tableName} `
+	entityCode=entityCode+`
+	}
+}`;
+	var returnValue={
+		entityCode:entityCode,
+		addSql:addFieldSql+addValueSql,
+		updateSql:updateSql,
+		deleteSql:` DELETE FROM [${tableName}] WHERE [Id] = @Id `,
+		queryAllSql:querySql,
+		queryOneSql:querySql+`${getNettRowStart}WHERE [Id] = @Id `
+	};
+	return returnValue;
+}
+
+function getRepositoryCode(tableNameWithoutFrontWord, tableName, fieldRegenerationInfo,namespace="ZOS.Common.Repository"){
+	
+	outPutRepositoryCode=`using ZOS.Common.Entities.ProduceManagement.PlanMarking.ProductionPlan;
+using ZOS.Common.IRepository.ProduceManagement.PlanMaking.ProductionPlanning;
+using ZOS.Data;
+using ZOS.Data.Repository;
+
+namespace ${namespace}
+{
+    public class  ${tableNameWithoutFrontWord}Repository : RepositoryBase<${tableNameWithoutFrontWord}>, I${tableNameWithoutFrontWord}Repository
+    {
+        public static SqlStrings ${tableNameWithoutFrontWord}Sql = new SqlStrings
+        {
+            TableName = "${tableName}",
+            Add = @"${fieldRegenerationInfo.addSql}",
+            Update = @"${fieldRegenerationInfo.updateSql}",
+            Delete = @"${fieldRegenerationInfo.deleteSql}",
+            QueryAll = @"${fieldRegenerationInfo.queryAllSql}",
+            QueryOne = @"${fieldRegenerationInfo.queryOneSql}"
+        };
+
+        public ${tableNameWithoutFrontWord}Repository(ISqlDatabaseProxy databaseProxy)
+            : base(databaseProxy)
+        {
+            Sql = ${tableNameWithoutFrontWord}Sql;
+        }
+        protected sealed override SqlStrings Sql
+        {
+            get { return base.Sql; }
+            set { base.Sql = value; }
+        }
+    }
+}
+
+	`;
+	return outPutRepositoryCode;
 }
 
 function addBlackSpace(fieldName){
@@ -49,7 +262,9 @@ function addBlackSpace(fieldName){
 function dealwithInputContent(content){
 	var analyseMap = getTableName(content);
 	analyseMap.fieldInfo=[];
+	analyseMap.fieldDescription={};
 	analyseMap = getFieldInfo(analyseMap);
+	analyseMap = getDescription(analyseMap);
 	
 	return analyseMap;
 }
@@ -109,6 +324,7 @@ function getFieldInfo(analyseMap){
 			fieldInfo.fieldIsNullAble = fieldIsNULLAble(content.substring(0,commaIndex));
 		}
 	}
+	//analyseMap.fieldInfo[fieldInfo.fieldName]=fieldInfo;
 	analyseMap.fieldInfo.push(fieldInfo);
 	analyseMap.restContent=content.substring(commaIndex+1).trim();
 	if(analyseMap.restContent.toUpperCase().indexOf(keyWords[8])===0 || analyseMap.restContent.indexOf(keyWords[9])===0){
@@ -118,12 +334,11 @@ function getFieldInfo(analyseMap){
 	}
 }
 
-function getDescription(content){
-
+function getDescription(analyseMap){
+	var content = analyseMap.restContent;
 	var descriptionInfo = {
 		description:"",
-		fieldName:"",
-		isColumnOrTable:""
+		fieldName:""
 	};
 	var firstDescription = content.indexOf(keyWords[10]);
 	content = content.substring(firstDescription+keyWords[10].length).trim();
@@ -131,20 +346,56 @@ function getDescription(content){
 	var description = content;
 	if(firstDescription>-1){
 		secondDescription = content.indexOf(keyWords[10]);
-		description = content.substring(0, secondDescription).trim();
-		// 从注释行中获取字段描述
+		if(secondDescription>-1){
+			description = content.substring(0, secondDescription).trim();
+			descriptionInfo.description="";
+			analyseMap.restContent = content.substring(secondDescription).trim();
+		}else{
+			description = content;
+			descriptionInfo.description="";
+			analyseMap.restContent = "";
+		}
 		
-		descriptionInfo.description="";
-
-		content = content.substring(secondDescription).trim();
+	}else{
+		return analyseMap;
 	}
+
+	// 从注释行中获取字段描述 description
+	descriptionInfo.description = getSpecialFieldInfoFromDescription(description, keyWords[11]);
+	if(descriptionInfo.description!=null){
+		// 获取字段名
+		descriptionInfo.fieldName = getSpecialFieldInfoFromDescription(description, keyWords[12]);
+		if(descriptionInfo.fieldName == null){
+			descriptionInfo.fieldName=keyWords[14];
+		}
+	}
+	analyseMap.fieldDescription[descriptionInfo.fieldName]=descriptionInfo.description;
+	analyseMap = getDescription(analyseMap);
+	return analyseMap;
+}
+
+function getSpecialFieldInfoFromDescription(description, keyWord){
+	var valueIndex = description.indexOf(keyWord);
+	if(valueIndex>-1){
+		var includeValueString = description.substring(valueIndex+keyWord.length).trim();
+		var leftQuotationMarkIndex = includeValueString.indexOf(keyWords[13]);
+		if(leftQuotationMarkIndex>-1){
+			var onlyRightQuotationMarkString = includeValueString.substring(leftQuotationMarkIndex+keyWords[13].length);
+			var rightQuotationMarkIndex = onlyRightQuotationMarkString.indexOf(keyWords[13]);
+			if(rightQuotationMarkIndex>-1){
+				return onlyRightQuotationMarkString.substring(0,rightQuotationMarkIndex);
+			}
+		}
+
+	}
+	return null;
 }
 
 function fieldIsNULLAble(content){
 	indexOfNOTNULL = content.indexOf("NOT NULL");
 	if(indexOfNOTNULL>-1){
-		return false;
+		return "";
 	}else{
-		return true;
+		return "?";
 	}
 }
